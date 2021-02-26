f8a83a299708d52f06b691bfd9103b49c2ab4915
abdulatifkhan <anvarkhanov96@gmail.com>
2021-02-26 17:43:40 +0500

    Add scripts

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
-let result = fizzBuzz('Qalay!')
-console.log(result);
+var elFizzBuzzForm = document.querySelector('.fizzbuzz-form')
+var elFizzBuzzButton = document.querySelector('.form__button');
+var elFizzBuzzResult = document.querySelector('.form-result');
+var FIZZ = 3;
+var BUZZ = 5;
 
+elFizzBuzzForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
 
-function fizzBuzz(input) {
-  if (typeof input !== 'number') 
-    return 'Not a number';
-
-  if ((input % 3 === 0) && (input % 5 ===0)) 
-    return 'fizzBuzz';
-   
-  if (input % 3 === 0) 
-    return 'fizz';
-   
-  if (input % 5 === 0) 
-    return 'buzz';
-  
-  return input;
-}
+  var elFizzBuzzInput = parseFloat(Math.floor(document.querySelector('.form__input').value.trim()), 10);
+  console.log(elFizzBuzzInput);   
+  if (isNaN(elFizzBuzzInput)) {
+    return elFizzBuzzResult.textContent = 'Not a number';
+  }
+  else if ((elFizzBuzzInput % FIZZ === 0) && (elFizzBuzzInput % BUZZ === 0)){
+    return elFizzBuzzResult.textContent = 'fizzBuzz';
+  }
+  else if (elFizzBuzzInput % FIZZ === 0){
+    return elFizzBuzzResult.textContent = 'fizz';
+  }
+  else if (elFizzBuzzInput % BUZZ === 0) {
+    return elFizzBuzzResult.textContent = 'buzz';
+  }
+  else {
+    return elFizzBuzzResult.textContent = 'bo\'linmaydi';
+  }
+})
