d702f36feb0bf67d71fd13b50b4625feb12ddb9b
abdulatifkhan <anvarkhanov96@gmail.com>
2021-02-26 15:28:13 +0500

    Add script file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let result = fizzBuzz('Qalay!')
+console.log(result);
+
+
+function fizzBuzz(input) {
+  if (typeof input !== 'number') 
+    return 'Not a number';
+
+  if ((input % 3 === 0) && (input % 5 ===0)) 
+    return 'fizzBuzz';
+   
+  if (input % 3 === 0) 
+    return 'fizz';
+   
+  if (input % 5 === 0) 
+    return 'buzz';
+  
+  return input;
+}
